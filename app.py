b9213df02a68cffdf8562e3efede5ab88de13168
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-04-04 16:42:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from flask import Flask
+app = Flask(__name__)
 
-app = FastAPI()
+@app.route('/')
+def hello_world():
+    return 'TechVJ'
 
-@app.get("/") def read_root(): return {"message": "Hello, Koyeb!"}
 
-if name == "main": import uvicorn uvicorn.run(app, host="0.0.0.0", port=8000)
-
+if __name__ == "__main__":
+    app.run()
