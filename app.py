6051c285b62194adaf8454ea88bf4f78a628cc9b
Shreeraam23 <161189756+Shreeraam23@users.noreply.github.com>
2025-04-04 16:01:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-worker: python3 channelbot.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'TechVJ'
+
+
+if __name__ == "__main__":
+    app.run()
